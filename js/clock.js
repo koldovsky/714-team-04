d1ca6c5941445813ebfc3334dfd22ce09d77529d
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-09-17 17:40:42 +0300

    fix var color

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    const clock = document.querySelector('.clock');
+    console.log(clock);
+    clock.innerText = new Date().toLocaleTimeString();
+}
+setInterval(updateClock, 1000);
+
+function updateDate() {
+    const date = document.querySelector('.date');
+    date.innerText = new Date().toLocaleDateString();
+}
+setInterval(updateDate);
