9c90dec8e67fe3904442600bfde0aa89790c1076
sergey-maslov <shift.tfihs@gmail.com>
2022-09-29 11:01:11 +0000

    clients added to JS

---
+++ js/testimonials-carousel.js
@@ -0,0 +1,32 @@
+const clients = [
+    {
+        id: 1,
+        name: "Josh Mendel",
+        picture: "img/josh_mendel.png",
+        feedback: "I really love to drink coffee in the morning, but I hate making it. Therefore, I decided to rent a coffee machine from this company. I am very pleased with the service - everything was done quickly, they brought and set up the coffee machine.",
+        date: "December 15, 2021",
+    },
+    {
+        id: 2,
+        name: "Jessica Malacks",
+        picture: "img/jessica_malacks.png",
+        feedback: "We have renovated the office kitchen and decided to delight our employees with automatic coffee machine. However, such equipment is very expensive. So, we simply rented coffee machine from this company and were completely satisfied.",
+        date: "February 10, 2022",
+    },
+    {
+        id: 3,
+        name: "Daniella Williamson",
+        picture: "img/daniella_williamson.png",
+        feedback: "When I opened a coffee shop, I couldn't find an affordable coffee machine. Finally, I found this company and I am grateful for their excellent advice. They helped with the choice of the coffee machine and regularly deliver coffee beans and cups.",
+        date: "December 15, 2021",
+    },
+    {
+        id: 4,
+        name: "Dean Steinberg",
+        picture: "img/dean_steinberg.png",
+        feedback: "We have been cooperating with this company for 2 years already, and during this time, not a single problem situation has arisen. The service is fast, the coffee is delivered without delay, the prices for coffee machine rental are very low.",
+        date: "March 22, 2021",
+    },
+] 
+
+let review = document.querySelector('.review');
